2f5b12d41498070c32a50f55f10754986be2beec
Ethan Chen <intervigil@gmail.com>
2018-12-01 13:53:00 -0800

    sunfish: Declare EUICC permission
    Change-Id: I1b151021ca9fc6cf35141b2ccb337bc83197089e

--- device-statix.mk
+++ device-statix.mk
@@ -4,5 +4,9 @@
 # Copyright (C) 2021 Sourajit Karmakar
 #
 
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-statix
