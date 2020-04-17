1762e0789b1d4afee70eb50e25bb8a02f84ff520
Chirayu Desai <chirayudesai1@gmail.com>
2020-04-17 19:30:31 +0530

    sunfish: Inline kernel building
    Change-Id: I1cc3ab1ff45a18d686107a7495b80777b769820f

--- BoardConfig-hentai.mk
+++ BoardConfig-hentai.mk
@@ -3,3 +3,13 @@
 # Copyright (C) 2021 StatiXOS.
 # Copyright (C) 2021 Sourajit Karmakar
 #
+
+# Kernel
+BOARD_KERNEL_IMAGE_NAME := Image.lz4
+KERNEL_LD := LD=ld.lld
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := sunfish_defconfig
+TARGET_KERNEL_SOURCE := kernel/google/sunfish
+TARGET_NEEDS_DTBOIMAGE := true
