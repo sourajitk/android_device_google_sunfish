db8a344b4e540d09563d1ae0b3a4c00bfa3a3a4a
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2021-06-30 08:06:27 -0400

    sunfish: Build display packages
    * Let's also inherit the commonsys-intf namespace as the
      display HAL depends on it for packages like libqdMetaData.
    
    * While at it, let's define the EGL props needed for the display
      HAL to initialize core renderer components.
    
    Signed-off-by: Sourajit Karmakar <sourajit.karmakar@gmail.com>

--- device-hentai.mk
+++ device-hentai.mk
@@ -3,3 +3,34 @@
 # Copyright (C) 2021 StatiXOS.
 # Copyright (C) 2021 Sourajit Karmakar
 #
+
+# Display
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/commonsys-intf/display \
+
+PRODUCT_PACKAGES += \
+    libqdMetaData \
+    libqdMetaData.vendor \
+    libdisplayconfig \
+    libdisplayconfig.qti.vendor \
+    libqdutils \
+    vendor.display.config@1.0.vendor \
+    vendor.display.config@1.1.vendor \
+    vendor.display.config@1.2.vendor \
+    vendor.display.config@1.3.vendor \
+    vendor.display.config@1.4.vendor \
+    vendor.display.config@1.5.vendor \
+    vendor.display.config@1.6.vendor \
+    vendor.display.config@1.7.vendor \
+    vendor.display.config@1.8 \
+    vendor.display.config@1.8.vendor \
+    vendor.display.config@1.9.vendor \
+    vendor.display.config@2.0.vendor \
+    vendor.qti.hardware.display.allocator@3.0.vendor:64 \
+    vendor.qti.hardware.display.mapper@2.0.vendor \
+    vendor.qti.hardware.display.mapper@3.0.vendor \
+    vendor.qti.hardware.display.mapperextensions@1.0.vendor \
+    vendor.qti.hardware.display.mapperextensions@1.1.vendor
+
+# Properties
+TARGET_VENDOR_PROP := device/google/sunfish/vendor.prop
